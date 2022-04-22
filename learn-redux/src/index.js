f6494fc73bffde2c15399e365a2b60ffb000d378
louis-25 <20151577@vision.hoseo.edu>
2022-04-22 15:16:10 +0900

    Redux 개발자도구 적용

--- learn-redux/src/index.js
+++ learn-redux/src/index.js
@@ -7,8 +7,9 @@ import './exercise'
 import { createStore } from 'redux';
 import { Provider } from 'react-redux'; // 어떤 컴포넌트에서도 리덕스 스토어에 접근할 수 있게됨
 import rootReducer from './modules';
+import { composeWithDevTools } from 'redux-devtools-extension'; // 리덕스 개발자 도구
 
-const store = createStore(rootReducer); // 스토어를 만듭니다.
+const store = createStore(rootReducer, composeWithDevTools()); // 스토어를 만듭니다.
 console.log(store.getState()); // 스토어의 상태를 확인해봅시다.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
